c6626648b460e2e44465a8d792f20d9574e3eaba
Yasser Shalash <yasservanshalash@gmail.com>
2024-02-05 19:07:28 +0100

    .-.

--- p_swap.c
+++ p_swap.c
@@ -2,83 +2,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int has_string (long numbers[])
+int	has_string(long numbers[])
 {
-    int i = 0;
-    int valid = 0;
+	int	i;
+	int	valid;
 
-    while(numbers[i])
-    {
-        if (!(numbers[i] > -2147483648 && numbers[i] < 2147483647))
-            valid = 1;
-        i++;
-    }
-    return valid;
+	i = 0;
+	valid = 0;
+	while (numbers[i])
+	{
+		if (!(numbers[i] > -2147483648 && numbers[i] < 2147483647))
+			valid = 1;
+		i++;
+	}
+	return (valid);
 }
 
-int count_strings(char **array_of_strings) {
-    int count = 0;
+int	count_strings(char **array_of_strings)
+{
+	int	count;
 
-    while (array_of_strings[count] != NULL) {
-        count++;
-    }
-
-    return count;
+	count = 0;
+	while (array_of_strings[count] != NULL)
+	{
+		count++;
+	}
+	return (count);
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    char **numbers_as_strings = NULL;
-    long numbers[argc - 1];
-    int i;
-    int j;
+	char	**numbers_as_strings;
+	long	numbers[argc - 1];
+	int		i;
+	int		j;
 
-    i = 1;
-    j = 0;
-    if (argc < 2) {
-        write(1, "Error\n", 6);
-        return (1);
-    }
-    else if(argc == 2)
-    {
-        numbers_as_strings = ft_split(argv[1], ' ');
-        j = count_strings(numbers_as_strings);
-    }
-    else
-    {
-        numbers_as_strings = (char **)malloc((argc - 1) * sizeof(char *));
-        while (argv[i])
-        {
-            numbers_as_strings[j] = malloc(strlen(argv[i]) + 1);
-            numbers_as_strings[j] = ft_strdup(argv[i]);
-            i++;
-            j++;
-        }
-    }
-    i = 0;
-    while (i < j)
-    {
-        numbers[i] = ft_atoi(numbers_as_strings[i]);
-        i++;
-    }
-    
-    // print numbers
-    i = 0;
-    while (i < j)
-    {
-        if (has_string(numbers) != 0)
-        {
-            printf("Error parsing numbers");
-            return (1);
-        }
-        else
-            printf("%ld\n", numbers[i]);
-        i++;
-    }
-    i = 0;
-    while (i < j)
-        free(numbers_as_strings[i++]);
-    free(numbers_as_strings);
-
-    return (0);
+	numbers_as_strings = NULL;
+	i = 1;
+	j = 0;
+	if (argc < 2)
+	{
+		write(1, "Error\n", 6);
+		return (1);
+	}
+	else if (argc == 2)
+	{
+		numbers_as_strings = ft_split(argv[1], ' ');
+		j = count_strings(numbers_as_strings);
+	}
+	else
+	{
+		numbers_as_strings = (char **)malloc((argc - 1) * sizeof(char *));
+		while (argv[i])
+		{
+			numbers_as_strings[j] = malloc(strlen(argv[i]) + 1);
+			numbers_as_strings[j] = ft_strdup(argv[i]);
+			i++;
+			j++;
+		}
+	}
+	i = 0;
+	while (i < j)
+	{
+		numbers[i] = ft_atoi(numbers_as_strings[i]);
+		i++;
+	}
+	// print numbers
+	i = 0;
+	while (i < j)
+	{
+		if (has_string(numbers) != 0)
+		{
+			printf("Error parsing numbers");
+			return (1);
+		}
+		else
+			printf("%ld\n", numbers[i]);
+		i++;
+	}
+	i = 0;
+	while (i < j)
+		free(numbers_as_strings[i++]);
+	free(numbers_as_strings);
+	return (0);
 }
