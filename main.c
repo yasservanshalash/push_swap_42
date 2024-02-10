0ffe3f66a4dc1f411561978dfc156bb1aa28bc27
Yasser Shalash <yasservanshalash@gmail.com>
2024-02-10 20:13:53 +0100

    checkpoint

--- main.c
+++ main.c
@@ -5,7 +5,7 @@ int	main(int argc, char **argv)
 {
 	long			*numbers_array;
 	int				size;
-	struct Stack	*a;
+	struct s_stack	*a;
 
 	numbers_array = return_numbers(argc, argv, &size);
 	a = array_to_stack(numbers_array, size);
